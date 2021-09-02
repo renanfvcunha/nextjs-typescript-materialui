fd4292b840e98054fd6480467bda0a6ac2a9bb75
Renan Cunha <rfvcunha@hotmail.com>
2021-09-02 11:51:10 -0300

    Adjusting template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { useEffect } from 'react';
 import { CssBaseline } from '@material-ui/core';
+
+import Seo from '../components/Seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -12,7 +15,22 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <CssBaseline /> <Component {...pageProps} />
+      <Head>
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Seo
+        metaTitle="NextJS With TS and Material UI"
+        metaDescription="NextJS Template With Typescript and Material-UI"
+      />
+      <CssBaseline />
+      <Component {...pageProps} />
     </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,19 +31,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <title>NextJS With TS and Material UI</title>
-          <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <meta
-            name="description"
-            content="NextJS Template With Typescript and Material-UI"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
